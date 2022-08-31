bcb7bdbf6ca16e959764aca776bc81a344ea21ae
gu <gu@lqq.com>
2022-08-31 10:15:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author guyunfei
+ * @create 2022-08-31 10:09
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
